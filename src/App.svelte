408a040fe64055f9c83ddf39da6d75623a81557f
Ford, Sam Michael <samford@sams-macbook-pro.local>
2023-11-20 16:35:25 -0500

    Working with notes and sounds and title

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,22 @@
 <script>
+  /* setup */
+  let apiKey = "";
+
+  /* consts */
+  const polySynth = new Tone.PolySynth(Tone.Synth).toDestination();
+  const notes = [ "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+
+  /* global vars */
   let chords = ["G", "Em", "C", "D"];
   let chord = "";
   let explanation = "";
+  $: notesInChords = new Set(chords.map(chord => chordToNotes(chord)).flat());
 
-  function play() {
-	const polySynth = new Tone.PolySynth(Tone.Synth).toDestination();
+  let rotatedNotes = notes;
 
+  /* Sound functions */
+
+  function playAll() {
 	let now = Tone.now()
 	for (let [idx, chord] of chords.entries()) {
 		let notes = chordToNotes(chord).map(note => note + "4");
@@ -13,11 +24,18 @@
 	}
   }
 
-  function chordToNotes(chord) {
-    return _chordToNotes[chord].map((note) => new Note(note));
+  function playChord(idx) {
+	let toPlay = chords[idx];
+	polySynth.triggerAttackRelease(chordToNotes(toPlay).map(note => note + "4"), "4n");
   }
 
-  let notes = [ "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+  function playNote(idx) {
+	let toPlay = notes[idx];
+	polySynth.triggerAttackRelease(toPlay + "4", "4n");
+  }
+
+
+  /* Music theory functions */
 
   function majTriad(root) { return [root, notes[(notes.indexOf(root) + 4) % 12], notes[(notes.indexOf(root) + 7) % 12]] };
   function minTriad(root) { return [root, notes[(notes.indexOf(root) + 3) % 12], notes[(notes.indexOf(root) + 7) % 12]] };
@@ -26,7 +44,6 @@
   function min7(root) { return [root, notes[(notes.indexOf(root) + 3) % 12], notes[(notes.indexOf(root) + 7) % 12], notes[(notes.indexOf(root) + 10) % 12]] };
   function dom7(root) { return [root, notes[(notes.indexOf(root) + 4) % 12], notes[(notes.indexOf(root) + 7) % 12], notes[(notes.indexOf(root) + 10) % 12]] };
 
-  // create mapping from chord name (note + type) to notes
   function createChordToNotes() {
 	let chordToNotes = {};
 
@@ -41,43 +58,26 @@
 	return chordToNotes;
   }
 
+
+  const _chordToNotes = createChordToNotes();
+
+  function chordToNotes(chord) {
+    return _chordToNotes[chord];
+  }
+
+  /* UI helpers */
+
+  function getHue(note) {
+	let idx = notes.indexOf(note);
+	return Math.sin(idx/12) * 360;;
+  }
+
+  /* Button handlers */
+
   function handleAddChord() {
     chords.push(chord);
     chords = chords;
     chord = "";
-  }
-
-  let _chordToNotes = createChordToNotes();
-
-  function getHue(chord) {
-	let idx = notes.indexOf(chord[0]);
-	return Math.sin(idx/12) * 360;;
-  }
-
-  async function sendRequest(prompt) {
-    let apiKey = "Insert here";
-
-    let req = {
-      method: "POST",
-      headers: {
-        Authorization: "Bearer " + apiKey,
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: prompt }],
-        max_tokens: 50,
-        temperature: 0.7,
-      }),
-    };
-
-    return fetch("https://api.openai.com/v1/chat/completions", req)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        return data;
-      });
   }
 
   function handleClear() {
@@ -91,8 +91,18 @@
 	};
   }
 
+  function rotateLeftOnce() {
+	rotatedNotes = [...rotatedNotes.slice(1), rotatedNotes[0]];
+  }
+
+  function rotateRightOnce() {
+	rotatedNotes = [rotatedNotes[rotatedNotes.length - 1], ...rotatedNotes.slice(0, rotatedNotes.length - 1)];
+  }
+
+  /* GPT  handlers */
+
   async function handleJazzify() {
-    let prompt = `Jazzify the following chord progression: ${chords.join("")}. 
+    let prompt = `Jazzify the following chord progression: ${chords.join("")}.
 	Return the result as a comma separated list of chords only. Do not respond with anything except the chords.`;
 
 	chords = (await sendRequest(prompt)).choices[0].message.content.split(",").map(chord => chord.trim()).map(chord => chord.replace("min", "m"));
@@ -107,12 +117,12 @@
   }
 
   async function handleExplain() {
-	let prompt = `Explain the following chord progression: ${chords.join("")}. 
+	let prompt = `Explain the following chord progression: ${chords.join("")}.
 	Be concise, but provide enough information to understand the progression.`;
 	let data = await sendRequest(prompt);
 
 	explanation = data.choices[0].message.content;
-  } 
+  }
 
   async function handleDarken() {
 	let prompt = `Darken the following chord progression: ${chords.join("")}.
@@ -135,22 +145,62 @@
 	chords = (await sendRequest(prompt)).choices[0].message.content.split(",").map(chord => chord.trim()).map(chord => chord.replace("min", "m"));
   }
 
+  /* GPT helpers */
+  async function sendRequest(prompt) {
+    let req = {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + apiKey,
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: prompt }],
+        max_tokens: 50,
+        temperature: 0.7,
+      }),
+    };
 
-
+    return fetch("https://api.openai.com/v1/chat/completions", req)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        return data;
+      });
+  }
 </script>
 
-<div class="min-h-screen flex items-center justify-center">
+<div class="min-h-screen flex items-center justify-center p-0">
   <div class="bg-gray-10 p-8 rounded-lg shadow-lg max-w-xl w-full">
+	<div class="flex items-center justify-center">
+	  <h2 class="text-1xl font-bold mb-4 text-center items-center justify-center bg-gradient-to-r from-red-400 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">A I M P R O V I S E</h2>
+	</div>
     <form on:submit|preventDefault={() => null} class="flex justify-center mb-4 space-x-4">
       <input bind:value={chord} placeholder="Add Chord" id="add-chord" class="bg-gray-100 hover:bg-gray-200 text-black py-2 px-4 rounded" />
       <button on:click={handleAddChord} id="add-chord" class="bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded" >Add Chord</button>
-      <button on:click={play} class="bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded" >Play</button>
+      <button on:click={() => playAll()} class="bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded" >Play</button>
 	</form>
     <div class="flex flex-wrap justify-center mb-4 space-x-4">
       {#each chords as chord, idx}
-        <button on:click={handleRemoveChord(idx)}  class="my-2 p-2 w-20 font-bold rounded" style="background-color: hsl({getHue(chord)}, 100%, 75%)">{chord}</button>
+	  <div class="relative group">
+        <button on:click={() => playChord(idx)}  class="border-none my-2 p-2 w-20 font-bold rounded" style="background-color: hsl({getHue(chord[0])}, 100%, 75%)">{chord}</button>
+        <button on:click={handleRemoveChord(idx)}  class="absolute top-0 right-[-8px] p-2 w-4 h-4 font-bold text-gray-700 rounded-full bg-white flex items-center justify-center font-mono opacity-0 group-hover:opacity-100">x</button>
+	  </div>
       {/each}
-    </div>
+	</div>
+    <ul class="flex justify-center space-x-2 mb-4">
+		<button on:click={() => rotateLeftOnce()} class="border-none font-bold inline list-none rounded-full bg-white-300 w-6 h-6 items-center flex justify-center">{"‹"}</button>
+		{#each rotatedNotes as note, idx}
+			{#if notesInChords.has(note)}
+				<button on:click={() => playNote(idx)} class="border-none font-bold inline list-none rounded-full w-6 h-6 items-center flex justify-center" style="background-color: hsl({getHue(note)}, 100%, 75%)">{note}</button>
+			{:else}
+				<button on:click={() => playNote(idx)} class="border-none inline list-none rounded-full bg-white-300 w-6 h-6 items-center flex justify-center">{note}</button>
+			{/if}
+		{/each}
+		<button on:click={() => rotateRightOnce()} class="border-none font-bold inline list-none rounded-full bg-white-300 w-6 h-6 items-center flex justify-center">{"›"}</button>
+	</ul>
+
     <div class="flex flex-wrap justify-center">
       <button on:click={handleJazzify} id="jazzify" class="m-2 bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded" >Jazzify</button>
       <button on:click={handleDarken} id="jazzify" class="m-2 bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded" >Darken</button>
@@ -163,5 +213,8 @@
 	<!-- <div class="flex justify-center">
 		<textarea bind:value={explanation} class="m-2 p-2 font-bold bg-gray-200 rounded" />
 	</div> -->
+  </div>
+  <div class="fixed bottom-1">
+    <input bind:value={apiKey} type="password" placeholder="OpenAI API KEY" class="bg-gray-100 hover:bg-gray-200 text-black py-2 px-4 rounded text-center" />
   </div>
 </div>
